11c3189ace487b1fc754f5bf625555239e673fdd
EliasHazielDelgadilloCalleros <al45429@un.edu.mx>
2024-11-05 13:20:39 -0600

    Actualización de IDAI paginas

---
+++ Archivos html/Ejemplo JavaScript/js/index.js
@@ -0,0 +1,8 @@
+console.log("Hola mundo desde mi archivo");
+alert("Esto es un DM para el mundo")
+
+console.log(5+360)
+console.warn("Estamos dando tips a los alumnos de IDAI")
+console.info("ESTO ES MUY IMPORTANTE")
+console.error("NO debi de haber estudiado psicologia")
+
